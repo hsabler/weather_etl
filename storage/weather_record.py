8ed9dc6848cfe4f7db10f542837b07cec255610e
HannaSabler <annik2810@gmail.com>
2025-10-27 22:10:40 +0300

    Выполнено: - Инкапсулирование WeatherAPIClient: логирование перенесено в соответсвующий метод (api_client) - Добавлено логирование в weather_record.py

--- storage/weather_record.py
+++ storage/weather_record.py
@@ -1,21 +1,38 @@
 import pandas as pd
+import logging
+import os
 
 class CsvRecord:
     """
-        Класс для сохранения данных о погоде в CSV-файл
+    Класс для сохранения данных о погоде в CSV-файл
 
-        Методы:
-            - save: сохраняет pandas DataFrame в CSV с указанным именем файла
-        """
+    Методы:
+        - save: сохраняет pandas DataFrame в CSV с указанным именем файла
+    """
+
+    def __init__(self):
+        self.logger = logging.getLogger(__name__)  # Инициализация логгера
+
     def save(self, df: pd.DataFrame, file_path: str):
         """
-               Сохраняет DataFrame в CSV-файл
+        Сохраняет DataFrame в CSV-файл
 
-               Args:
-                   df (pd.DataFrame): Таблица с данными о погоде или любыми другими данными
-                   file_path (str): Путь и имя файла для сохранения (например, 'weather_data.csv')
+        Args:
+            df (pd.DataFrame): Таблица с данными о погоде или любыми другими данными
+            file_path (str): Путь и имя файла для сохранения (например, 'weather_data.csv')
 
-               Returns:
-                   None
-               """
-        df.to_csv(file_path, index=False)
+        Returns:
+            None
+        """
+        # Папка для сохранения данных
+        data_folder = "data"
+
+        # Формируем полный путь для сохранения файла в папке 'data'
+        full_file_path = os.path.join(data_folder, file_path)
+
+        try:
+            self.logger.info(f"Попытка сохранить данные в файл: {full_file_path}")
+            df.to_csv(full_file_path, index=False)  # Сохраняем данные в файл
+            self.logger.info(f"Данные успешно сохранены в файл: {full_file_path}")
+        except Exception as e:
+            self.logger.error(f"Ошибка при сохранении данных в файл {full_file_path}: {e}")
